95a79bdd3e2036bd3f4965b500a77521fef2a868
kong dong <1216181067@qq.com>
2019-05-30 18:05:14 +0800

    添加http 工具库和cache 库

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import FillToAspectRatio from "./react-native-aufit-camera" ;
 import StyleUtils from "./react-native-autofit-size" ;
 import { RNAlert,Alert } from "./components/Alert" ;
 import { RNPicker,Picker } from "./components/Picker" ;
-import Cache from "./cache" ;
+import { storage,memory } from "./cache" ;
 import parseUrl from "./http" ;
 export FillToAspectRatio from "./react-native-aufit-camera" ;
 export StyleUtils from "./react-native-autofit-size" ;
@@ -11,5 +11,5 @@ export default {
     RNAlert, Alert,
     RNPicker, Picker,
     FillToAspectRatio,StyleUtils,
-    Cache,parseUrl,
+    storage,memory,parseUrl,
 }
